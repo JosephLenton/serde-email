6e8e11ebeaf441957386c681d3ccd3dd1b9f36f7
josephlenton@gmail.com <josephlenton@gmail.com>
2022-11-20 20:37:00 +0100

    chore: add docs for is_valid_email

--- src/is_valid_email.rs
+++ src/is_valid_email.rs
@@ -1,6 +1,9 @@
 use ::email_address::EmailAddress;
 use ::std::convert::AsRef;
 
+/// Tests if the given string is a valid email or not.
+///
+/// The underlying implementation is by the [email_address crate](https://crates.io/crates/email_address).
 pub fn is_valid_email<S>(raw: S) -> bool
 where
     S: AsRef<str>,
